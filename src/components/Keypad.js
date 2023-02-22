d0ade6c94863d3b9547488d95b035429599d00f1
Samantha Genevay <samanthagenevay@gmail.com>
2023-02-22 15:09:38 -0800

    Completed Lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad () {
+    
+    const handlePasswordChange = (e) => {
+        console.log("Entering password...");
+    }
+    
     return (
-        <div></div>
+        <div>
+            <input 
+                type="password"
+                onChange={handlePasswordChange}
+                placeholder="Enter your password"
+            />
+        </div>
     )
 }
 
